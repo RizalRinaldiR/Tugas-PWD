3f2eee0cece139eb677ed14ac1462e25cbb13939
RizalRinaldiR <164452019+RizalRinaldiR@users.noreply.github.com>
2024-03-22 10:27:23 +0700

    Update skrol.js

--- Gabungan Website Terbaik Rizal Rinaldi Ramadhan/skrol.js
+++ Gabungan Website Terbaik Rizal Rinaldi Ramadhan/skrol.js
@@ -1,14 +1,14 @@
-var scrollUpBtn = document.getElementById ("atas");
+// var scrollUpBtn = document.getElementById ("atas");
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        atas.style.display = "block";
-    } else {
-        atas.style.display = "none";
-    }
-};
+// window.onscroll = function() {
+//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//         atas.style.display = "block";
+//     } else {
+//         atas.style.display = "none";
+//     }
+// };
 
-function scrollToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; 
-}
+// function scrollToTop() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0; 
+// }
